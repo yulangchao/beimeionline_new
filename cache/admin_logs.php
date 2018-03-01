4fb39b0606b20bb59828a178b8bb6ea647102d32
Richard <richardyych@gmail.com>
2018-03-02 04:58:25 +0800

    first commit

---
+++ cache/admin_logs.php
@@ -0,0 +1,3 @@
+<?php	
+$logdb[]='1	admin	1519937468	50.98.121.11	http://155.254.49.132/admin/index.php?lfj=city&job=listsort	http://155.254.49.132/admin/index.php?lfj=city&action=addsort';
+$logdb[]='1	admin	1519937369	50.98.121.11	http://155.254.49.132/admin/index.php?lfj=center&job=config	http://155.254.49.132/admin/index.php?lfj=center&action=config';
